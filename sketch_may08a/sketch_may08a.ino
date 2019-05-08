e37d53674c016011ce21fa9bdc3b6927937b6738
jean72027 <50391443+jean72027@users.noreply.github.com>
2019-05-08 11:47:23 +0800

    Add files via upload

---
+++ sketch_may08a/sketch_may08a.ino
@@ -0,0 +1,22 @@
+
+int butpin=2;
+int ledpin=13;
+int butstate=0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(butpin,INPUT);
+pinMode(ledpin,OUTPUT);
+digitalWrite(butpin,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+butstate=digitalRead(butpin);
+if(butstate==HIGH){
+  digitalWrite(ledpin,HIGH);
+  }
+  else{
+    digitalWrite(ledpin,LOW);
+    }
+}
